e0d15edadbd23794258de4201ccf69eb8b071ff5
Tox- <gau.vui97@hotmail.com>
2022-01-27 18:18:04 +0100

    Add log

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,8 +3,12 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.dao.VehicleDAO;
 import com.parkit.parkingsystem.model.Ticket;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FareCalculatorService {
+
+    private static final Logger logger = LogManager.getLogger("FareCalculatorService");
 
     private VehicleDAO vehicleDao;
 
@@ -42,7 +46,8 @@ public class FareCalculatorService {
 
     private double calcRemise(double price, String vehicleRegNumber) {
         double remise = 0;
-        if(!vehicleDao.isFirstUseOfParking(vehicleRegNumber)) {
+        if(!vehicleDao.isFirstUseOfParking(vehicleRegNumber) && price > 0.0) {
+            logger.info("Your a regular user, we offer to you 5 percent reduction on the total cost of your ticket");
             remise = (price / 100) * 5;
         }
         return price - remise;
